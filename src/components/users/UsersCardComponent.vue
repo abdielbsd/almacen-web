b6233c0355d9db6a066a4aecd638451edad1146f
Abdiel Blanco Serrano <ablancosg@gmail.com>
2023-01-20 19:55:31 -0500

    multiple_changes_channel

--- src/components/users/UsersCardComponent.vue
+++ src/components/users/UsersCardComponent.vue
@@ -1,81 +1,9 @@
 <template>
 <div>
 
-    <v-switch
-      v-model="ifViewform"
-      :label="ifViewform ? 'Create' : 'Edit'"
-    ></v-switch>
-  <v-card class="overflow-hidden" color="primary lighten-1" dark cols="12" v-show="!ifViewform">
-    <v-toolbar flat color="primary">
-      <v-icon>mdi-account</v-icon>
-      <v-toolbar-title class="font-weight-light">
-        {{userView.name}}
-      </v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-btn
-        color="purple darken-3"
-        fab
-        small
-        @click="(isEditing = !isEditing), (iseliminated = false)"
-      >
-        <v-icon v-if="isEditing && this.userView !== null"> mdi-close </v-icon>
-        <v-icon v-else> mdi-pencil </v-icon>
-      </v-btn>
-      <v-btn
-        color="red darken-3"
-        fab
-        small
-        @click="(iseliminated = !iseliminated), (isEditing = false)"
-      >
-        <v-icon v-if="iseliminated && this.userView !== null">
-          mdi-close
-        </v-icon>
-        <v-icon v-else> mdi-delete </v-icon>
-      </v-btn>
-    </v-toolbar>
-    <v-card-text>
-      <v-text-field
-        v-model="userView.name"
-        color="white"
-        label="Name"
-        required
-      ></v-text-field>
-      <v-text-field
-        v-model="userView.email"
-        :rules="emailRules"
-        label="E-mail"
-        required
-      ></v-text-field>
-      <v-autocomplete
-        v-model="userView.role"
-        :items="rol"
-        :filter="customFilter"
-        color="white"
-        item-text="name"
-        label="Role"
-      ></v-autocomplete>
-    </v-card-text>
-    <v-divider></v-divider>
-    <v-card-actions>
-      <v-spacer></v-spacer>
-      <v-btn color="success" @click="save">
-        {{ control }}
-      </v-btn>
-    </v-card-actions>
-    <v-snackbar v-model="hasSaved.state" :timeout="2000" absolute bottom left>
-         {{hasSaved.msg}}
-    </v-snackbar>
-  </v-card>
-  
-   <v-card class="overflow-hidden" color="primary lighten-1" dark cols='12'   v-show="ifViewform">
-    <v-toolbar flat color="primary">
-      <v-icon>mdi-account</v-icon>
-      <v-toolbar-title class="font-weight-light">
-        {{userView.name}}
-      </v-toolbar-title>
-      <v-spacer></v-spacer>
 
-    </v-toolbar>
+  
+   <v-container>
     <v-card-text>
       <v-text-field
         v-model="userView.name"
@@ -110,17 +38,7 @@
         label="Role"
       ></v-autocomplete>
     </v-card-text>
-    <v-divider></v-divider>
-    <v-card-actions>
-      <v-spacer></v-spacer>
-      <v-btn color="success" @click="agregate">
-        add
-      </v-btn>
-    </v-card-actions>
-    <v-snackbar v-model="hasSaved.state" :timeout="2000" absolute bottom left>
-         {{hasSaved.msg}}
-    </v-snackbar>
-  </v-card>
+   </v-container>
   </div>
 </template>
 <script>
--- src/store/users/actions.js
+++ src/store/users/actions.js
@@ -4,12 +4,12 @@ import { getToken } from '@/utils/utils';
 axiosInstance.defaults.headers.common['x-token'] = getToken()
 
 export async function lister(__, parm) {
-  console.log("jhbjhljhu", parm)
+  //console.log("jhbjhljhu", parm)
   const res = await axiosInstance.get(`/user?limit=${parm.limit}&skip=${parm.skip}`);
   return res.data;
 }
 export async function update_user(__, parm) {
-  console.log("jhbjhljhu", parm)
+  //console.log("jhbjhljhu", parm)
   const res = await axiosInstance.put(`/user/${parm.uid}`, {
     role: parm.role,
     name: parm.name,
@@ -17,13 +17,13 @@ export async function update_user(__, parm) {
   return res.data;
 }
 export async function remove_user(__, parm) {
-  console.log("remve", parm)
+  //console.log("remve", parm)
   const res = await axiosInstance.delete(`/user/${parm.uid}`);
   return res.data;
 }
 
 export async function add_user(__, {parm,psw}) {
-  console.log("jhbjhljhu", parm,psw)
+  //console.log("jhbjhljhu", parm,psw)
   const res = await axiosInstance.post(`/user/`, {
     role: parm.role,
     name: parm.name,
@@ -32,5 +32,4 @@ export async function add_user(__, {parm,psw}) {
   });
   return res.data;
 }
-
 
